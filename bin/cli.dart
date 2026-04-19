aa6dc27c002aab3078703d4a26be0f405513476e
puce.edgar <puce.edgar@gmail.com>
2026-04-19 22:25:08 +0900

    init commit

---
+++ bin/cli.dart
@@ -0,0 +1,61 @@
+import 'package:cli/cli.dart' as cli;
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
+
+
+void main(List<String> arguments) async {
+  var runner = CommandRunner();
+  await runner.run(arguments);
+  // if (arguments.isEmpty) {
+  //   print('Hello Dart!');
+  // } else if (arguments.first == 'version') {
+  //   print('Version: ${cli.version()}');
+  // } else if (arguments.first == 'hi') {
+  //   cli.sayHi();
+  // } else if (arguments.first == 'wikipedia') {
+  //   final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+  //   searchWikipedia(inputArgs);
+  // } else {
+  //   print('Unknown arg passed!');
+  // }
+}
+
+void searchWikipedia(List<String>? arguments) async {
+  final String articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    final inputFromStdin = stdin.readLineSync(); // Read input
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('No article title provided. Exiting.');
+      return; // Exit the function if no valid input
+    }
+    articleTitle = inputFromStdin;
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  // print('Here ya go!');
+  // print('(Pretend this is an article about "$articleTitle")');
+
+  var articleContent = await getWikiArticle(articleTitle);
+  print(articleContent);
+}
+
+Future<String> getWikiArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org',
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+
+  // Return an error message if the request failed
+  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
+}
---
+++ lib/cli.dart
@@ -0,0 +1,11 @@
+int calculate() {
+  return 6 * 7;
+}
+
+void sayHi() {
+  print("Hi there!");
+}
+
+String version() {
+  return '1.0.0';
+}
